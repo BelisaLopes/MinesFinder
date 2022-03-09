f3b3d50ff579bd5d7f060521c06fb6f9cc10e1a3
Belisa Lopes <2200724@my.ipleiria.pt>
2022-03-09 10:07:35 +0000

    adicionar janela

--- src/main/java/pt/ipleiria/estg/es1/minesfinder/MinesFinder.java
+++ src/main/java/pt/ipleiria/estg/es1/minesfinder/MinesFinder.java
@@ -1,4 +1,17 @@
 package pt.ipleiria.estg.es1.minesfinder;
 
-public class MinesFinder {
+import javax.swing.*;
+
+public class MinesFinder extends JFrame {
+
+    private JPanel painelPrincipal;
+
+    public MinesFinder(String title) {
+        super(title);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(painelPrincipal);
+    }
+    public static void main(String[] args) {
+        new MinesFinder("Mines Finder").setVisible(true);
+    }
 }
